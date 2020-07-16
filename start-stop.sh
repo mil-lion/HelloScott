1d27812b231d4d8df136c1a53cdca74414e3aa0c
Igor Morenko <morenko@lionsoft.ru>
2020-07-16 13:57:04 +0300

    Доработан скрипт запуска/остановки сервера

--- start-stop.sh
+++ start-stop.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+#JAVA_HOME=
 JAR="HelloScott-0.0.1-SNAPSHOT.jar"
 #OPTS="-Xmx48m -Xss256k"
 PID="application.pid"
-JAVA=java
+JAVA=$JAVA_HOME/bin/java
 
 case $1 in
         start)
@@ -15,14 +16,14 @@ case $1 in
                       fi
                 fi
                 
-                nohup $JAVA -jar $OPTS $JAR /tmp 2>> /dev/null >> /dev/null &
+                nohup $JAVA $OPTS -jar $JAR /tmp 2>> /dev/null >> /dev/null &
                         echo $! > $PID
                 echo "Started"
         ;;
         stop)
                 if [ -f $PID ]; then
                         PIDN=$(cat $PID);
-                        echo "Stopping"
+                        echo "Stopping..."
                         kill $PIDN;
                         echo "Stopped"
                         rm $PID
@@ -34,11 +35,18 @@ case $1 in
                 if [ -f $PID ] && [ -n $PID ]; then
                         if ps -p `cat $PID` > /dev/null; then
                                 echo "Running"
+                                exit 0
                         else
                                 echo "Not running"
+                                exit 1
                         fi
                 else
                         echo "Not running"
+                        exit 1
                 fi
+        ;;
+        *)
+                echo "Usage: start-stop.sh {start|stop|status}"
+                exit 3
         ;;
 esac
